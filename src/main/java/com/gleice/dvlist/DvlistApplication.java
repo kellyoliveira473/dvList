fac34dcec0fb2dc2b303aa8a434aa52ba6d7e02e
kellyoliveira473 <kellyoliveira473@gmail.com>
2025-08-21 23:49:39 -0300

    Projeto criado

---
+++ src/main/java/com/gleice/dvlist/DvlistApplication.java
@@ -0,0 +1,13 @@
+package com.gleice.dvlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gleice/dvlist/DvlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gleice.dvlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
