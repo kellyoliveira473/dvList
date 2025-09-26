4a61a93284d48e17062a9aabc511924df0f8fac1
kellyoliveira473 <kellyoliveira473@gmail.com>
2025-09-26 20:08:34 -0300

    Properties ,Game,ORM

---
+++ src/main/java/com/kelly/dslist/entities/Game.java
@@ -0,0 +1,39 @@
+package com.kelly.dslist.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name ="tb_game" )
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platform;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return Objects.equals(id, game.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
