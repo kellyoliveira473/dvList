28a2e2ab246f5c595aec7689b6b5e2a7454da417
kellyoliveira473 <kellyoliveira473@gmail.com>
2025-10-01 09:00:13 -0300

    Atualizando o insert

--- src/main/java/com/kelly/dslist/entities/GameList.java
+++ src/main/java/com/kelly/dslist/entities/GameList.java
@@ -16,7 +16,7 @@ public class GameList {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nome;
+    private String name;
 
     @Override
     public boolean equals(Object o) {
